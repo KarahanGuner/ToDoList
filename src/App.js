3590f61f18f79dd7150385b5865a738eac307bde
Kara6000 <aqwkarahann@gmail.com>
2020-09-02 18:38:10 +0300

    added custom console log messages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import {Paper} from '@material-ui/core';
 
 import HomePage from './pages/homepage/homepage.component';
+import CustomLogging from './CustomLogging';
+
+const custom = new CustomLogging();
+custom.log('Hello there!');
+custom.log('This website has been created by Karahan Guner as a portfolio project.');
+custom.log('If you have any suggestions or if you would like to hire me you can contact me at: kkarahanguner@gmail.com');
 
 const App = () => {
   return (
