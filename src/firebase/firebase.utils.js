3d09bf2c26f0514500e0613b26e4c7b9928b03df
Kara6000 <aqwkarahann@gmail.com>
2020-09-23 20:16:39 +0300

    deleting the firebase key

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBopCcUzk7G_4BdtUFnSiN20-zOqjzzq_w",
-    authDomain: "listappkara-175e3.firebaseapp.com",
-    databaseURL: "https://listappkara-175e3.firebaseio.com",
-    projectId: "listappkara-175e3",
-    storageBucket: "listappkara-175e3.appspot.com",
-    messagingSenderId: "236880475531",
-    appId: "1:236880475531:web:fc072453474f4e7449ab8e",
-    measurementId: "G-7TZ5Y328K7"
+    //firebase key
 };
 
 
